811ead356ffd5d0506b3803851f47adf579ea87b
theKazantsev <djacidk@gmail.com>
2023-08-19 15:29:18 +1000

    init commit

---
+++ src/test/java/RegistrationTest.java
@@ -0,0 +1,12 @@
+import org.junit.jupiter.api.Test;
+
+import static com.codeborne.selenide.Selenide.*;
+
+public class RegistrationTest {
+
+    @Test
+    public  void registrationTest() {
+        open("https://qa-mesto.praktikum-services.ru/");
+    }
+
+}
